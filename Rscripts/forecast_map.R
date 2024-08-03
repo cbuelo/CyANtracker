8c84b90d02b1cdd093521732ed19c447ee4ab01b
cbuelo <cbuelo10@gmail.com>
2024-08-03 08:54:57 -0500

    GH actions working! clean up script, change schedule back to weekly

--- Rscripts/forecast_map.R
+++ Rscripts/forecast_map.R
@@ -30,13 +30,8 @@ if(!cur_date %in% prev_dates){
   library(pandoc)
   
   pd_loc = pandoc::pandoc_install()
-  print("tried finding pandoc")
-  print(pd_loc)
-  print("!!!")
-  # rmarkdown::find_pandoc(pd_loc)
   pandoc::pandoc_activate()
   # format table
-  print("got to forecast_table formatting")
   forecast_table = forecast_table |> 
     rename(lake = `Lake Name`,
            chance_cyanoHAB = `% Chance of CyanoHAB`,
@@ -48,19 +43,13 @@ if(!cur_date %in% prev_dates){
            label = paste(lake, paste0(chance_cyanoHAB, "%"), sep=" </br> ")) |> 
     rename(date_range = Date)
   # save table
-  print("got to writing")
   write_csv(forecast_table, file.path("Data", paste0(cur_date, ".csv")))
-  print("before plot_map")
   # make and save map
   plot_map(forecast_table, cur_date)
-  print("after plot_map")
   # re-render the index file to push to pages
   startpath = Sys.getenv("PATH")
-  hold_pandoc_loc = pandoc::pandoc_bin() # rmarkdown::find_pandoc()
-  print(hold_pandoc_loc)
+  hold_pandoc_loc = pandoc::pandoc_bin()
   Sys.setenv(PATH = paste(startpath, hold_pandoc_loc, sep=":"))
-  print("PATH set")
   rmarkdown::render("index.Rmd")
-  print("Ran render")
   Sys.setenv(PATH = startpath)
 }
